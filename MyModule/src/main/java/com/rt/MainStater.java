81a865ef3ddd7ae172eca4ed35a81c5eefb0f60e
wenxinghui <wenxinghui@feiniu.com>
2020-06-08 15:22:01 +0800

    提交——hot_fix01

--- MyModule/src/main/java/com/rt/MainStater.java
+++ MyModule/src/main/java/com/rt/MainStater.java
@@ -15,5 +15,6 @@ public class MainStater {
         AnnotationConfigApplicationContext a = new AnnotationConfigApplicationContext(SpringTestConfig.class);
         Person person = a.getBean(Person.class);
         System.out.println(person);
+        System.out.println("hot_fix");
     }
 }
